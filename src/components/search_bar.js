4e8e90386859181cff927d9da97c8d9cf1a34184
Nicole Chow <nicolechowt@gmail.com>
2018-01-30 12:34:12 -0800

    first commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+//every class must have a render function
+class SearchBar extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { term: "" };
+	}
+// everytime we update or setState is called, the render method gets run as well,
+// whatever is in render will be pushed to the DOM
+	render() {
+		return (
+			<div>
+				<input 
+				value={this.state.term}
+				onChange={event => this.setState({ term: event.target.value })} />
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
---
+++ src/components/video_list.js
@@ -0,0 +1,29 @@
+import React from "react";
+import VideoListItem from "./video_list_item";
+
+// props is being passed from index.js, "videos" was passed 
+// as an argument, "videos" is basically the data coming
+// back from the API
+
+// .map is looping through the videos array from index.js
+// video represents each element in the array
+// return the VideoListItem component, and we are passing in 
+// video as a prop(erty), and we will pass each element as an 
+// argument
+
+
+// videoItems is an array of components
+
+
+const VideoList = (props) => {
+	const videoItems = props.videos.map((video) => {
+		return <VideoListItem key={video.etag} video ={video} />
+	})
+	return (
+		<ul className="col-md-4 list-group">
+			{videoItems}
+		</ul>
+	);
+};
+
+export default VideoList;
