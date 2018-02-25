827f33b17473443afd1e44d8dced1b58285988c3
Nicole Chow <nicolechowt@gmail.com>
2018-02-24 20:14:32 -0800

    configured mLab for deployment

--- server/server.js
+++ server/server.js
@@ -6,10 +6,18 @@ const path = require('path');
 const ItemModel = require('./models/gratitudeListModel');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
-// const routes = require('./routes/gratitudeListRoutes');
+const configDB = require('../config/database');
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Gratitudedb');
+mongoose.connect(configDB.url);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
